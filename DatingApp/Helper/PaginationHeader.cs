020abf5ae607d1f412a744e684ebe84047392c6f
limon-7 <liibd7@gmail.com>
2022-02-05 18:52:22 +0600

    Fixed: Pagination header spelling

--- DatingApp/Helper/PaginationHeader.cs
+++ DatingApp/Helper/PaginationHeader.cs
@@ -3,15 +3,15 @@ namespace DatingApp.Helper
     public class PaginationHeader
     {
         public int CurrentPage { get; set; }
-        public int IteamsPerPage { get; set; }
-        public int TotalIteams { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalItems { get; set; }
         public int TotalPages { get; set; }
 
-        public PaginationHeader(int currentPage, int iteamsPerPage, int totalIteams, int totalPages)
+        public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
         {
             this.CurrentPage = currentPage;
-            this.IteamsPerPage = iteamsPerPage;
-            this.TotalIteams = totalIteams;
+            this.ItemsPerPage = itemsPerPage;
+            this.TotalItems = totalItems;
             this.TotalPages = totalPages;
         }
     }
